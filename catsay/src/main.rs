fdb2c3d9a296451c1cb17c4d0a073e0528f313f4
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-10 18:16:02 +0300

    feat: add 'dead' cat logic

--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -5,18 +5,25 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Options {
     #[structopt(default_value = "I'm cat!")]
-    // comments started with /// are documentation comments 
+    // comments started with /// are documentation comments
     /// What does the cat say?
-    message: String
+    message: String,
+    #[structopt(short = "d", long = "dead")]
+    /// Makes cat appear dead
+    dead: bool,
 }
 
 fn main() {
     let options = Options::from_args();
-    let message = options.message;
+    let mut message = options.message;
+
+    let eye = if options.dead { "x" } else { "o" };
+    message = if options.dead { "Ooops...X__x".to_string() } else { message };
+
     println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("     /\\_/\\");
-    println!("    ( o o )");   
+    println!("    ( {eye} {eye} )", eye=eye);
     println!("    =( I )=");
 }
